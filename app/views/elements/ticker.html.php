4d9c71158a877afa571b971cd14cb34d91a9b358
Nilam Doctor <nilamdoc@gmail.com>
2012-12-18 13:41:28 +0530

    ticker

--- app/views/elements/ticker.html.php
+++ app/views/elements/ticker.html.php
@@ -6,16 +6,21 @@ $tickers = Tickers::find('first',array(
 			)
 ));
 ?>
-<table class="table">
+<table class="table table-condensed table-striped">
 
 <?php
 foreach(compact('tickers') as $key=>$val){
 ?>
-<tr><td>
-<?php
-	print_r( $val['ticker']['avg']);
-?>
-</td></tr>
+<tr><th>Date: <?=date('Y-m-d H:i:s e',$val['date']->sec)?></th></tr>
+<tr><td>High: <?=$val['ticker']['high']?></td></tr>
+<tr><td>Low: <?=$val['ticker']['low']?></td></tr>
+<tr><td>Avg: <?=$val['ticker']['avg']?></td></tr>
+<tr><td>WAvg: <?=$val['ticker']['vwap']?></td></tr>
+<tr><td>Vol: <?=$val['ticker']['vol']?></td></tr>
+<tr><td>Last: <?=$val['ticker']['last']?></td></tr>
+<tr><td>Buy: <?=$val['ticker']['buy']?></td></tr>
+<tr><td>Sell: <?=$val['ticker']['sell']?></td></tr>
+
 <?php
 }
 ?>
