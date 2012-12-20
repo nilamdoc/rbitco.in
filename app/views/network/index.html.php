a23d6ecf809c45e98f993a480848879f60801982
Nilam Doctor <nilamdoc@gmail.com>
2012-12-20 14:28:23 +0530

    net

--- app/views/network/index.html.php
+++ app/views/network/index.html.php
@@ -1,7 +1,7 @@
 <div>
 <strong>Network status: </strong>We are in sync with bitcoin network!
 <hr>
-<h2><a href="/networks/blocks"><?=$getblockcount?> Blocks</a></h2>
+<h2><a href="/network/blocks"><?=$getblockcount?> Blocks</a></h2>
 Generated on <?=date('Y-m-d H:i:s',$getblock['time']);?>.
 The above block had difficulty level of <?=$getblock['difficulty']?>.<br>
 Hash: <?=$getblock['hash']?>
