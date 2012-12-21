ba8a32e6c135db72081cd3bcdfff3aedeb3b9a0f
Nilam Doctor <nilamdoc@gmail.com>
2012-12-21 12:58:44 +0530

    ticker

--- app/views/elements/ticker.html.php
+++ app/views/elements/ticker.html.php
@@ -12,14 +12,14 @@ $tickers = Tickers::find('first',array(
 foreach(compact('tickers') as $key=>$val){
 ?>
 <!-- <tr><th>Date: <?=date('Y-m-d H:i:s e',$val['date']->sec)?></th></tr> -->
-<tr><td>High:</td><td> $<?=round($val['ticker']['high'],3)?></td></tr>
-<tr><td>Low:</td><td> $<?=round($val['ticker']['low'],3)?></td></tr>
-<tr><td>Avg:</td><td> $<?=round($val['ticker']['avg'],3)?></td></tr>
-<tr><td>WAvg:</td><td> $<?=round($val['ticker']['vwap'],3)?></td></tr>
-<tr><td>Vol:</td><td> <?=round($val['ticker']['vol'],2)?></td></tr>
-<tr><td>Last:</td><td> $<?=round($val['ticker']['last'],3)?></td></tr>
-<tr><td>Buy:</td><td> $<?=round($val['ticker']['buy'],3)?></td></tr>
-<tr><td>Sell:</td><td> $<?=round($val['ticker']['sell'],3)?></td></tr>
+<tr><td>High:</td><td> $<?=str_pad(round($val['ticker']['high'],4),7,"0",STR_PAD_RIGHT)?></td></tr>
+<tr><td>Low:</td><td> $<?=str_pad(round($val['ticker']['low'],4),7,"0",STR_PAD_RIGHT)?></td></tr>
+<tr><td>Avg:</td><td> $<?=str_pad(round($val['ticker']['avg'],4),7,"0",STR_PAD_RIGHT)?></td></tr>
+<tr><td>WAvg:</td><td> $<?=str_pad(round($val['ticker']['vwap'],4),7,"0",STR_PAD_RIGHT)?></td></tr>
+<tr><td>Vol:</td><td align="right"> <?=round($val['ticker']['vol'],2)?></td></tr>
+<tr><td>Last:</td><td> $<?=str_pad(round($val['ticker']['last'],4),7,"0",STR_PAD_RIGHT)?></td></tr>
+<tr><td>Buy:</td><td> $<?=str_pad(round($val['ticker']['buy'],4),7,"0",STR_PAD_RIGHT)?></td></tr>
+<tr><td>Sell:</td><td> $<?=str_pad(round($val['ticker']['sell'],4),7,"0",STR_PAD_RIGHT)?></td></tr>
 <?php
 }
 ?>
