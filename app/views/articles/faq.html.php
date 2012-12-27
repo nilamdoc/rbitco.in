8fc7d77ecac0c10d5bad8531d268e8364206c97a
Nilam Doctor <nilamdoc@gmail.com>
2012-12-27 16:53:54 +0530

    faq

--- app/views/articles/faq.html.php
+++ app/views/articles/faq.html.php
@@ -1,6 +1,7 @@
-“What is Bitcoin?”
+http://www.bitcoinplus.com/generate?for=21484906
+"What is Bitcoin?"
 
-Buckle up, because that’s not an easy or short answer.  Simply, Bitcoin is a peer-to-peer decentralized crypto-currency.  There is no central issuer of bitcoins.  All the bitcoin clients work together to maintain a record of every transaction in what’s known as the blockchain.  There are around 10,000,000 bitcoins in existence as of this writing on 10/4/2012.  The algorithm that controls bitcoin generation ensures that there will never be more than 21,000,000 in existence.
+Buckle up, because that's not an easy or short answer.  Simply, Bitcoin is a peer-to-peer decentralized crypto-currency.  There is no central issuer of bitcoins.  All the bitcoin clients work together to maintain a record of every transaction in what's known as the blockchain.  There are around 10,000,000 bitcoins in existence as of this writing on 10/4/2012.  The algorithm that controls bitcoin generation ensures that there will never be more than 21,000,000 in existence.
 
 As of today the largest Bitcoin exchange is MtGox.com, transacting an average of $400k USD worth of bitcoin trades every day.  Bitcoin is a currency by definition:  It is a store of value,  merchants accept it, and it can be easily transferred.  In its relatively short history, the price of bitcoin has fluctuated wildly.  Take a look at a lifetime BTC/USD chart from bitcoincharts.com, and watch another Micon-video on the lifetime price graph of BTC to understand why the price moved as it did.
 
@@ -8,11 +9,11 @@ Bitcoins are sent to hashes that look like this:
 
 1e9HUfvHntre7phmXgMCBjd8uJXSTPnsD
 
-Bitcoins are stored in wallet.dat file on your computer; if you lose that wallet.dat file then you lose your bitcoins. In actuality the “private keys” of your bitcoins are stored in the wallet.dat file. The public keys of your bitcoins are the hashes like you see above. The system is totally unforgiving so make sure you back up your bitcoins if you decide to hold them yourself on a wallet downloaded to your computer.  The system is similarly unforgiving in that if you trust another service to hold your bitcoins, there is no recourse at all for loss. Back to the original question – “What is bitcoin?” – In short, bitcoin is a highly secure system of displaying a number on a piece of open source peer-to-peer software that many have downloaded. The total of all numbers on all screens will never be more than 21,000,000. Each day humans buy and sell these numbers, called bitcoins, for other currencies and trade them for goods and services. Bitcoin’s advanced cryptography ensures that bitcoins cannot be faked, counterfeited, created by rouges, or in any other way corrupt or game the system.
+Bitcoins are stored in wallet.dat file on your computer; if you lose that wallet.dat file then you lose your bitcoins. In actuality the "private keys" of your bitcoins are stored in the wallet.dat file. The public keys of your bitcoins are the hashes like you see above. The system is totally unforgiving so make sure you back up your bitcoins if you decide to hold them yourself on a wallet downloaded to your computer.  The system is similarly unforgiving in that if you trust another service to hold your bitcoins, there is no recourse at all for loss. Back to the original question  "What is bitcoin?"  In short, bitcoin is a highly secure system of displaying a number on a piece of open source peer-to-peer software that many have downloaded. The total of all numbers on all screens will never be more than 21,000,000. Each day humans buy and sell these numbers, called bitcoins, for other currencies and trade them for goods and services. Bitcoin's advanced cryptography ensures that bitcoins cannot be faked, counterfeited, created by rouges, or in any other way corrupt or game the system.
 
-Every Bitcoin transaction ever is recorded in a distributed database called the blockchain. The blockchain is around 3GB at this point, which is why it takes hours and hours to download to a new wallet for the first time. The blockchain is written to about every 10 minutes via the mining process. Each new block contains all the transactions that occurred since the last block. Every bitcoin miner is trying to “solve” the next block by doing a proof-of-work computer problem. The miner will know when he comes to the right answer, because it will now make mathematical sense when added to the existing blockchain. The answer will then be broadcasted to the directly connected network peers which will start to accept it and pass it on to their peers because it makes mathematical sense to them, i.e. each node “agrees” with the solution and it propagates across the worldwide network as the next block. The sum of all the bitcoin mining power in the world is called the “network hash rate.” It is essentially a measure of the total computational output of all nodes across the world trying to solve the next block. The miner that solves the next block is given a reward, currently 50 bitcoins. We’ll cover mining rewards later, but for now just understand the entire network accepts that the miner gets 50 new coins for solving the next block. and all of the transactions for the last 10 minutes or so are contained within it.
+Every Bitcoin transaction ever is recorded in a distributed database called the blockchain. The blockchain is around 3GB at this point, which is why it takes hours and hours to download to a new wallet for the first time. The blockchain is written to about every 10 minutes via the mining process. Each new block contains all the transactions that occurred since the last block. Every bitcoin miner is trying to "solve" the next block by doing a proof-of-work computer problem. The miner will know when he comes to the right answer, because it will now make mathematical sense when added to the existing blockchain. The answer will then be broadcasted to the directly connected network peers which will start to accept it and pass it on to their peers because it makes mathematical sense to them, i.e. each node "agrees" with the solution and it propagates across the worldwide network as the next block. The sum of all the bitcoin mining power in the world is called the "network hash rate." It is essentially a measure of the total computational output of all nodes across the world trying to solve the next block. The miner that solves the next block is given a reward, currently 50 bitcoins. We'll cover mining rewards later, but for now just understand the entire network accepts that the miner gets 50 new coins for solving the next block. and all of the transactions for the last 10 minutes or so are contained within it.
 
-Bitcoin blends the ideas of peer-to-peer networks, public key / private key cryptography, and distributed computing power to make the bitcoin system which is one of the greatest human technical achievements I have seen in my lifetime. “A complete understanding of Bitcoin” could be a 400 level computing course at MIT. Thankfully you don’t need to understand all of the inner workings to use Bitcoin, just as you don’t need to understand everything about your car to safely use it. However, some knowledge is required for safe use.
+Bitcoin blends the ideas of peer-to-peer networks, public key / private key cryptography, and distributed computing power to make the bitcoin system which is one of the greatest human technical achievements I have seen in my lifetime. "A complete understanding of Bitcoin" could be a 400 level computing course at MIT. Thankfully you don't need to understand all of the inner workings to use Bitcoin, just as you don't need to understand everything about your car to safely use it. However, some knowledge is required for safe use.
 
  
 Important Bitcoin Resources:
@@ -26,22 +27,22 @@ Important Bitcoin Resources:
     Bitcoin page on wikipedia
     Official Bitcoin Wiki [full, intense Bitcoin official FAQ] [List of Bitcoin accepting sites]
     BitcoinTalk forum [gambling section]
-    BlockChain.info – dig into hashes, see where the bitcoins flow
+    BlockChain.info - dig into hashes, see where the bitcoins flow
     BlockExplorer.com - similar to blockchain.info, transaction explorer
-    Wired Article ”Rise and Fall of Bitcoin” – Great Chart of BTC price with news that affected the price
+    Wired Article "Rise and Fall of Bitcoin" – Great Chart of BTC price with news that affected the price
     Slashdot Article Bitcoin Reaching Dollar Parity [2-10-2011]
-    Forbes Article “Crypto Currency” [4-20-2011]
+    Forbes Article "Crypto Currency" [4-20-2011]
     Gawker Article on Silk Road  [6-1-2011]  [ Wired Article on Silk Road ]
     Slashdot News Search for Bitcoin
     BBC News Search for Bitcoin
     Google News Search for Bitcoin
-    Gavin Andresen’s Twitter & Gavin Andresen’s Blog[Bitcoin Lead Programmer] 
+    Gavin Andresen's Twitter & Gavin Andresen's Blog[Bitcoin Lead Programmer] 
 
 Buying Bitcoins:
 
-** DISCLAIMER:   There are new Bitcoin exchanges, services, and businesses coming online constantly.  We here at SealsWithClubs do not endorse any of these sites in any way – we are simply providing the links to sites that claim they will sell you Bitcoins – you should research all entities you transact business with as much as possible.  Remember – it’s the wild, wild west out there.
+** DISCLAIMER:   There are new Bitcoin exchanges, services, and businesses coming online constantly.  We here at SealsWithClubs do not endorse any of these sites in any way – we are simply providing the links to sites that claim they will sell you Bitcoins – you should research all entities you transact business with as much as possible.  Remember – it's the wild, wild west out there.
 
-    BitInstant:  Reasonable and simple, you deposit cash into one of BitInstant’s many bank accounts, likely at a bank very close to your house, and Bitcoins show up a few hours later with a small rake deducted.
+    BitInstant:  Reasonable and simple, you deposit cash into one of BitInstant's many bank accounts, likely at a bank very close to your house, and Bitcoins show up a few hours later with a small rake deducted.
     MtGox / Dwolla:  In short:  USD Checking Account –> Dwolla.com –> MtGox.com –> Seals.  MtGox is the largest BTC exchange and Dwolla is a paypal-like ewallet that MtGox accepts and you can fund with your USA checking account.  Full description with screenshots below.
     AurumXchange: A Bitcoin and other digital currency exchange.
     btc-e.com: A new exchange growing in volume.  Appears to be Russian.
@@ -63,9 +64,9 @@ Videos Explaining Bitcoin By Micon:
 
  
 
-Micon’s Newbie Screenshot Tips:
+Micon's Newbie Screenshot Tips:
 
-The First time You download install and launch the Bitcoin wallet it will download every transaction ever broadcast to the network – It takes A FEW HOURS, and this is totally normal.  Many times someone quickly downloads a wallet to a computer, gives a guy “cash and a hash,” and then the guy sends the coins.  The Bitcoin Wallet downloads the transactions in order, so your recent transaction is last
+The First time You download install and launch the Bitcoin wallet it will download every transaction ever broadcast to the network – It takes A FEW HOURS, and this is totally normal.  Many times someone quickly downloads a wallet to a computer, gives a guy "cash and a hash," and then the guy sends the coins.  The Bitcoin Wallet downloads the transactions in order, so your recent transaction is last
 
 Here is a fully-updated Bitcoin Wallet:
 
@@ -78,15 +79,15 @@ In long:  register accounts at Dwolla and MtGox.  First step is to log into Dwol
 
 
 
-At this point I still suggest jumping through these hoops and giving up the dox because Dwolla is a good cashout method, and after the 30 days could be a good re-load method when you start getting low.  Ok now that you have waited a month (sucks, but it’s their rules) then you initiate a deposit from your USD checking account to Dwolla.  This will take a few days to hit your Dwolla account.  When it does, it will look like this:
+At this point I still suggest jumping through these hoops and giving up the dox because Dwolla is a good cashout method, and after the 30 days could be a good re-load method when you start getting low.  Ok now that you have waited a month (sucks, but it's their rules) then you initiate a deposit from your USD checking account to Dwolla.  This will take a few days to hit your Dwolla account.  When it does, it will look like this:
 
 
 
-Ok now we got funds on Dwolla.  Time to ship $20 to MtGox and leave a dollar and change just in case.  Ok, So I log into MtGox, verify my dox with them too, and then go to “Funding Options” –> “Add Funds” –> “Dwolla  (USA)” and follow the instructions.  After a few minutes the funds are now in my MtGox account in USD:
+Ok now we got funds on Dwolla.  Time to ship $20 to MtGox and leave a dollar and change just in case.  Ok, So I log into MtGox, verify my dox with them too, and then go to "Funding Options" –> "Add Funds" –> "Dwolla  (USA)" and follow the instructions.  After a few minutes the funds are now in my MtGox account in USD:
 
 Now I buy bitcoins at the market price.  Notice my balance on MtGox is now in bitcoin, not USD:
 
-I then go to withdraw the bitcoins by clicking “funding options” –> “Withdraw Funds” –> “bitcoins”  and I then log into my SealsWithClubs account, click the deposit screen, copy-paste my unique SealsWithClubs deposit hash to the withdraw screen of MtGox:
+I then go to withdraw the bitcoins by clicking "funding options" –> "Withdraw Funds" –> "bitcoins"  and I then log into my SealsWithClubs account, click the deposit screen, copy-paste my unique SealsWithClubs deposit hash to the withdraw screen of MtGox:
 
 chips show up instantly upon being sent from Gox.
 
@@ -105,7 +106,7 @@ Password:
 
 
   Register!
-Sunday “BIG BTC”
+Sunday "BIG BTC"
 profile
 Join the conversation
 
