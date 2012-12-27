5cd6301c16c681d05e49268feace220ba1cc1205
Nilam Doctor <nilamdoc@gmail.com>
2012-12-27 11:08:53 +0530

    bit

--- app/views/elements/footer.html.php
+++ app/views/elements/footer.html.php
@@ -1,7 +1,7 @@
 <?php
 use app\extensions\action\Functions;
 $functions = new Functions();		
-$wallet = $functions->getBitAddress('BitCoin');
+$wallet = $functions->getBitAddress('Bitcoin');
 ?>
 <div id="footer" style="padding:1px 20px; border-top:1px solid black" class="navbar-inner navbar ">
 	<ul class="nav" style="font-size:11px ">
