1b7c57c84f473988cd6229811d086fb0faac28de
Nilam Doctor <nilamdoc@gmail.com>
2013-01-09 21:00:28 +0530

    a

--- app/config/routes.php
+++ app/config/routes.php
@@ -64,7 +64,7 @@ Router::connect('/', 'Pages::view');
  * `PagesController`, rendering `/views/pages/about.html.php` as a static page.
  */
 Router::connect('/pages/{:args}', 'Pages::view');
-Router::connect('/s/{:args}', 'ShortURL::index');
+Router::connect('/s/{:args}', 'ShortUrl::index');
 
 /**
  * Add the testing routes. These routes are only connected in non-production environments, and allow
