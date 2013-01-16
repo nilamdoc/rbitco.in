93773e041c4b15a1524a97d744f0bfaac7b901b6
Nilam Doctor <nilamdoc@gmail.com>
2013-01-16 16:43:19 +0530

    ssl

--- app/views/elements/footer.html.php
+++ app/views/elements/footer.html.php
@@ -85,6 +85,8 @@ GlobeSSL Seal Logo Html Builder Code:
 Shows the logo at URL https://rbitcoin/img/corner-seal-new.gif
 Logo type is  ("GSSL")
 //-->
+<script language="JavaScript" src="https://static.globessl.com/seal.js" type="text/javascript">
+</script>  
 <style>#bottomright { position:fixed; bottom:0px; right:0px; width:100px; height:100px; z-index:999;}</style>
 <div id="bottomright">
 <script type="text/javascript">Seal("https://rbitco.in/img/corner-seal-new.gif", "GSSL", "bottomright");</script></div>
