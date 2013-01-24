f101bf9f6625b9bfafe950ba3e9b7429ea772cc0
Nilam Doctor <nilamdoc@gmail.com>
2013-01-24 18:45:45 +0530

    c

--- app/views/articles/payment.html.php
+++ app/views/articles/payment.html.php
@@ -1,12 +1,175 @@
 <h4>Payment options:</h4>
-<div style="padding:10px;background-color:#FFFFFF ">
-<h4><a name="USA"></a>If you are in the USA</h4>
-</div>
+<a name="USA"></a>
 <br>
+
+<div style="padding:10px;background-color:#FFFFFF ">
+<h4>If you are in the USA</h4>
+<div class="alert">
+<h4>Important instructions </h4>
+<h6>How will we know it's you who made the deposit?</h6>
+<p>Please round the amount of your order up to the next higher whole amount, then add the last two digits of the Invoice/Order Number to the amount, as cents. This unique deposit amount is how we can tell it was you who made the deposit.</p>
+    <p>If your order total is: $12.45 Round it up to: $13.00</p>
+    <p>Then, if your Invoice/Order Number is: RB-30045</p>
+    <p>Add $0.45 to the amount... ( the last two digits of your Order Number is 45 )</p>
+    <p>Making the final exact amount to deposit: $13.45 </p>
+</div>	
+<p>You will make the payment from your bank to the details given below.</p>
+<table class="table table-condensed table-striped table-bordered" style="background-color:white;width:70% ">
+	<tr>
+		<td>Bank Name:</td>
+		<th>ICICI Bank</th>
+	</tr>
+	<tr>
+		<td>Branch Name:</td>
+		<th>JMC House</th>
+	</tr>
+	<tr>
+		<td>Branch Address:</td>
+		<th>JMC HOUSE, OPP. PARIMAL GARDENS, OFF. C.G. ROAD</th>
+	</tr>
+	<tr>
+		<td>Pin/Zip code:</td>
+		<th>380 006</th>
+	</tr>
+	<tr>
+		<td>Branch City:</td>
+		<th>Ahmedabad</th>
+	</tr>
+	<tr>
+		<td>Swift Code:</td>
+		<th>ICICINBB024</th>
+	</tr>
+	<tr>
+		<td>Branch Country:</td>
+		<th>India IN</th>
+	</tr>
+	<tr>
+		<td>Account Name:</td>
+		<th>Hitarth Consultants</th>
+	</tr>
+	<tr>
+		<td>Account Number:</td>
+		<th>002405010974</th>
+	</tr>
+	<tr>
+		<td>MICR code:</td>
+		<th>380229002</th>
+	</tr>	
+</table>
+<p>You can make a payment directly by depositing cash, credit/debit card payment to the above bank details or you can visit <a href="http://www.icicibank.com/nri-banking/money_transfer/money2india.html"> Online transfer ICICI bank</a>.</p>
+</div>
+<a name="India"></a><br>
 <div style="padding:10px;background-color:#FFFFFF ">
 <h4><a name="India"></a>If you are in India</h4>
+<div class="alert">
+<h4>Important instructions </h4>
+<h6>How will we know it's you who made the deposit?</h6>
+<p>Please round the amount of your order up to the next higher whole amount, then add the last two digits of the Invoice/Order Number to the amount, as cents. This unique deposit amount is how we can tell it was you who made the deposit.</p>
+    <p>If your order total is: Rs.1122.45 Round it up to: $1223.00</p>
+    <p>Then, if your Invoice/Order Number is: RB-30048</p>
+    <p>Add Rs.0.48 to the amount... ( the last two digits of your Order Number is 48 )</p>
+    <p>Making the final exact amount to deposit: Rs.1223.48 </p>
+</div>	
+<p>You will make the payment from your bank to the details given below.</p>
+<table class="table table-condensed table-striped table-bordered" style="background-color:white;width:70% ">
+	<tr>
+		<td>Bank Name:</td>
+		<th>ICICI Bank</th>
+	</tr>
+	<tr>
+		<td>Branch Name:</td>
+		<th>JMC House</th>
+	</tr>
+	<tr>
+		<td>Branch Address:</td>
+		<th>JMC HOUSE, OPP. PARIMAL GARDENS, OFF. C.G. ROAD</th>
+	</tr>
+	<tr>
+		<td>Pin/Zip code:</td>
+		<th>380 006</th>
+	</tr>
+	<tr>
+		<td>Branch City:</td>
+		<th>Ahmedabad</th>
+	</tr>
+	<tr>
+		<td>Swift Code:</td>
+		<th>ICICINBB024</th>
+	</tr>
+	<tr>
+		<td>Branch Country:</td>
+		<th>India IN</th>
+	</tr>
+	<tr>
+		<td>Account Name:</td>
+		<th>Hitarth Consultants</th>
+	</tr>
+	<tr>
+		<td>Account Number:</td>
+		<th>002405010974</th>
+	</tr>
+	<tr>
+		<td>MICR code:</td>
+		<th>380229002</th>
+	</tr>	
+</table>
+<p>You can make a payment directly by depositing cash, credit/debit card payment to the above bank details or you can visit <a href="https://infinity.icicibank.co.in/web/services/jsp/Locateus.jsp"> Online transfer ICICI bank</a>.</p>
 </div>
+<a name="Other"></a>
 <br>
 <div style="padding:10px;background-color:#FFFFFF ">
-<h4><a name="Other"></a>If you are in any other country</h4>
+<h4>If you are in any other country</h4>
+<div class="alert">
+<h4>Important instructions </h4>
+<h6>How will we know it's you who made the deposit?</h6>
+<p>Please round the amount of your order up to the next higher whole amount, then add the last two digits of the Invoice/Order Number to the amount, as cents. This unique deposit amount is how we can tell it was you who made the deposit.</p>
+    <p>If your order total is: $12.45 Round it up to: $13.00</p>
+    <p>Then, if your Invoice/Order Number is: RB-30045</p>
+    <p>Add $0.45 to the amount... ( the last two digits of your Order Number is 45 )</p>
+    <p>Making the final exact amount to deposit: $13.45 </p>
+</div>	
+<p>You will make the payment from your bank to the details given below.</p>
+<table class="table table-condensed table-striped table-bordered" style="background-color:white;width:70% ">
+	<tr>
+		<td>Bank Name:</td>
+		<th>ICICI Bank</th>
+	</tr>
+	<tr>
+		<td>Branch Name:</td>
+		<th>JMC House</th>
+	</tr>
+	<tr>
+		<td>Branch Address:</td>
+		<th>JMC HOUSE, OPP. PARIMAL GARDENS, OFF. C.G. ROAD</th>
+	</tr>
+	<tr>
+		<td>Pin/Zip code:</td>
+		<th>380 006</th>
+	</tr>
+	<tr>
+		<td>Branch City:</td>
+		<th>Ahmedabad</th>
+	</tr>
+	<tr>
+		<td>Swift Code:</td>
+		<th>ICICINBB024</th>
+	</tr>
+	<tr>
+		<td>Branch Country:</td>
+		<th>India IN</th>
+	</tr>
+	<tr>
+		<td>Account Name:</td>
+		<th>Hitarth Consultants</th>
+	</tr>
+	<tr>
+		<td>Account Number:</td>
+		<th>002405010974</th>
+	</tr>
+	<tr>
+		<td>MICR code:</td>
+		<th>380229002</th>
+	</tr>	
+</table>
+<p>You can make a payment directly by depositing cash, credit/debit card payment to the above bank details or you can visit <a href="http://www.icicibank.com/nri-banking/money_transfer/money2india.html"> Online transfer ICICI bank</a>.</p>
 </div>
