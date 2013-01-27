1c3ef7d1a92dfdf58cd59130b36e891a0a56b49d
Nilam Doctor <nilamdoc@gmail.com>
2013-01-27 16:22:30 +0530

    backup

--- app/extensions/command/Backupwallet.php
+++ app/extensions/command/Backupwallet.php
@@ -12,7 +12,7 @@ class Backupwallet extends \lithium\console\Command {
     public function run() {
 		print_r(VANITY_OUTPUT_DIR.gmdate('Y-m-d-H',time()));
 		$bitcoin = new Bitcoin('http://'.BITCOIN_WALLET_SERVER.':'.BITCOIN_WALLET_PORT,BITCOIN_WALLET_USERNAME,BITCOIN_WALLET_PASSWORD);			
-		$backup = $bitcoin->backup(VANITY_OUTPUT_DIR.gmdate('Y-m-d-H',time()).'.xyz');
+		$backup = $bitcoin->backupwallet(VANITY_OUTPUT_DIR.gmdate('Y-m-d-H',time()).'.xyz');
 	}
 }
 ?>
