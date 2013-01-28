36e8d59066f4164b2c57b7a5061b4308a66ca471
Nilam Doctor <nilamdoc@gmail.com>
2013-01-28 16:27:09 +0530

    fix

--- app/views/transact/buysellaccept.mail.php
+++ app/views/transact/buysellaccept.mail.php
@@ -1,21 +1,21 @@
-<p>Hi <?=$data['username']?>, </p>
-<h5>Your order number is: RB<?=substr($data['accept']['datetime']['time'],4,1)?>00<?=substr($data['accept']['datetime']['time'],6,2)?> </h5>
+<p>Hi <?=$data['accept.username']?>, </p>
+<h5>Your order number is: RB<?=substr($data['accept.datetime.time'],4,1)?>00<?=substr($data['accept.datetime.time'],6,2)?> </h5>
 <p>You have accepted to <?=$params[10]?> <?=$params[5]?> BTC at <?=$params[6]?> <?=$params[7]?>. 
-Your total payment due is <?=$params[9]?>.<?=substr($data['accept']['datetime']['time'],6,2)?></p>
+Your total payment due is <?=$params[9]?>.<?=substr($data['accept.datetime.time'],6,2)?></p>
 
 <p>Your confirmation is very important. </p>
 <?php if($params[10]=="Buy"){?>
 <p>As this is a BUY-BTC order you will have to deposit <strong><?=$params[7]?> <?=round($params[5]*$params[6],0)?>.
-<?=substr($data['accept']['datetime']['time'],6,2)?></strong> as per the instructions below:</p>
+<?=substr($data['accept.datetime.time'],6,2)?></strong> as per the instructions below:</p>
 <p>As this is a trade between you and the seller, we need to escrow the amount before we complete the deal.</p>
 
 <h4>Important instructions </h4>
 <h4>How will we know it's you who made the deposit?</h4>
 <p>Please round the amount of your order up to the next higher whole amount, then add the last two digits of the Invoice/Order Number to the amount, as cents. This unique deposit amount is how we can tell it was you who made the deposit.</p>
     <p>Your order total is: <?=$params[7]?> <?=round($params[5]*$params[6],2)?> Round it up to: <?=$data['currency']?>  <?=round($params[5]*$params[6],0)?></p>
-    <p>if your Invoice/Order Number is: RB<?=substr($data['accept']['datetime']['time'],4,1)?>00<?=substr($data['accept']['datetime']['time'],6,2)?></p>
-    <p>Add <?=$params[7]?>  0.<?=substr($data['accept']['datetime']['time'],6,2)?> to the amount... ( the last two digits of your Order Number is <?=substr($data['accept']['datetime']['time'],6,2)?> )</p>
-    <p>Making the final exact amount to deposit: <?=$params[7]?> <?=round($params[5]*$params[6],0)?>.<?=substr($data['accept']['datetime']['time'],6,2)?> </p>
+    <p>if your Invoice/Order Number is: RB<?=substr($data['accept.datetime.time'],4,1)?>00<?=substr($data['accept.datetime.time'],6,2)?></p>
+    <p>Add <?=$params[7]?>  0.<?=substr($data['accept.datetime.time'],6,2)?> to the amount... ( the last two digits of your Order Number is <?=substr($data['accept.datetime.time'],6,2)?> )</p>
+    <p>Making the final exact amount to deposit: <?=$params[7]?> <?=round($params[5]*$params[6],0)?>.<?=substr($data['accept.datetime.time'],6,2)?> </p>
 </div>	
 <p>You will make the payment from your bank to the details given below.</p>
 <table style="background-color:white;width:70% ">
