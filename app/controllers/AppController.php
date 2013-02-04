e3829276add1dc91410c004609f10603c69ee5ff
Nilam Doctor <nilamdoc@gmail.com>
2013-02-04 20:41:40 +0530

    facebook

---
+++ app/controllers/AppController.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\controllers;
+
+use app\models\api;
+use app\extensions\action\OAuth2;
+use app\extensions\action\Functions;
+
+class AppController extends \lithium\action\Controller {
+
+	public function index(){
+	}
+
+	public function facebook(){
+	}
+
+}
+?>
