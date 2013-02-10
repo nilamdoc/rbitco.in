3fbd0849f96780faf77e6490b646ea083b4ef01f
Nilam Doctor <nilamdoc@gmail.com>
2013-02-10 19:41:20 +0530

    map

--- app/views/network/peer.html.php
+++ app/views/network/peer.html.php
@@ -27,8 +27,12 @@ array_multisort($getpeerinfo);
 		?>
 		<tr>
 			<td><?=$i?></td>		
-			<td><?=$ip_port[0]?></td>
-			<td><?=$ip_port[1]?></td>
+			<td><?=$ip_port[0]?><br>
+			<?=$peer['version']?>:<?=$peer['subver']?>/<?=$peer['banscore']?><br>
+			&lt; <?=$function->toFriendlyTime(gmdate(time())-$peer['lastsend'])?> &gt; <?=$function->toFriendlyTime(gmdate(time())-$peer['lastrecv'])?></td>
+			<td><?=$ip_port[1]?><br>
+			<?php print_r(IsTorExitPoint($ip_port[0],$ip_port[1]))?>
+			</td>
 			<td><?=$function->toFriendlyTime(gmdate(time())-$peer['conntime'])?></td>			
 			<td>
 				<?php echo $ip_location['jdec']['countryName'];?>:<?php echo $ip_location['jdec']['timeZone'];?><br>
@@ -43,3 +47,39 @@ array_multisort($getpeerinfo);
 		?>
 	</tbody>
 </table>
+
+<?php
+
+function IsTorExitPoint($ip,$port){
+    if (gethostbyname(ReverseIPOctets($ip).".".$port.".".ReverseIPOctets($ip).".ip-port.exitlist.torproject.org")=="127.0.0.2") {
+       return "Y";
+    } else {
+       return "N";
+    } 
+}
+
+function ReverseIPOctets($inputip){
+    $ipoc = explode(".",$inputip);
+    return $ipoc[3].".".$ipoc[2].".".$ipoc[1].".".$ipoc[0];
+}
+?>
+<script src="//maps.googleapis.com/maps/api/js?v=3&client=gme-1035482723370.apps.googleusercontent.com&sensor=false" type="text/javascript"></script>
+
+ <script type="text/javascript">
+    function initialize() {
+      var map = new google.maps.Map(
+        document.getElementById('map_canvas'), {
+          center: new google.maps.LatLng(37.4419, -122.1419),
+          zoom: 13,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+
+      var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(37.4419, -122.1419),
+            map: map
+      });
+
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+  <div id="map_canvas"></div>
