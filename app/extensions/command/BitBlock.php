828a6604ebee603af67a9a8ed05abe475d85182c
Nilam Doctor <nilamdoc@gmail.com>
2013-02-27 11:25:55 +0530

    blockcount

--- app/extensions/command/BitBlock.php
+++ app/extensions/command/BitBlock.php
@@ -9,26 +9,30 @@ class BitBlock extends \lithium\console\Command {
 
     public function run() {
 	$bitcoin = new Bitcoin('http://'.BITCOIN_WALLET_SERVER.':'.BITCOIN_WALLET_PORT,BITCOIN_WALLET_USERNAME,BITCOIN_WALLET_PASSWORD);
+	$getblockcount = $bitcoin->getblockcount();
 	
 	$height = Blocks::find('first',array(
 		'order' => array('height'=>'DESC')
 	));
+	
 	$h = (int)$height['height'] + 1;
 		for($i = $h;$i<=$h+20;$i++)	{
-			$getblockhash = $bitcoin->getblockhash($i);
-			$getblock = $bitcoin->getblock($getblockhash);
-			$txid = 0;
-			foreach($getblock['tx'] as $txx){
-				$getrawtransaction = $bitcoin->getrawtransaction((string)$txx);
-		//	print_r($getrawtransaction);
-				$decoderawtransaction = $bitcoin->decoderawtransaction($getrawtransaction);
-		//		print_r($decoderawtransaction);
-				
-				$getblock['txid'][$txid] = $decoderawtransaction;
-				$txid ++;
+			if($i <= $getblockcount){
+				$getblockhash = $bitcoin->getblockhash($i);
+				$getblock = $bitcoin->getblock($getblockhash);
+				$txid = 0;
+				foreach($getblock['tx'] as $txx){
+					$getrawtransaction = $bitcoin->getrawtransaction((string)$txx);
+			//	print_r($getrawtransaction);
+					$decoderawtransaction = $bitcoin->decoderawtransaction($getrawtransaction);
+			//		print_r($decoderawtransaction);
+					
+					$getblock['txid'][$txid] = $decoderawtransaction;
+					$txid ++;
+				}
+				Blocks::create()->save($getblock);
+			//	print_r($getblock);
 			}
-			Blocks::create()->save($getblock);
-		//	print_r($getblock);
 		}
 	}
 }
