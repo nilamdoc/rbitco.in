92d8e07148ffcde99e4147ae52ea838d20b9cf67
Nilam Doctor <nilamdoc@gmail.com>
2013-02-27 11:11:25 +0530

    blocks

---
+++ app/extensions/command/BitBlock.php
@@ -0,0 +1,35 @@
+<?php
+namespace app\extensions\command;
+use app\models\Blocks;
+use app\extensions\action\Bitcoin;
+
+//every 2 seconds cron job for adding transactions....
+
+class BitBlock extends \lithium\console\Command {
+
+    public function run() {
+	$bitcoin = new Bitcoin('http://'.BITCOIN_WALLET_SERVER.':'.BITCOIN_WALLET_PORT,BITCOIN_WALLET_USERNAME,BITCOIN_WALLET_PASSWORD);
+	
+	$height = Blocks::find('first',array(
+		'order' => array('height'=>'DESC')
+	));
+	$h = (int)$height['height'] + 1;
+	
+	$getblockhash = $bitcoin->getblockhash($h);
+	$getblock = $bitcoin->getblock($getblockhash);
+	$txid = 0;
+	foreach($getblock['tx'] as $txx){
+		$getrawtransaction = $bitcoin->getrawtransaction((string)$txx);
+//	print_r($getrawtransaction);
+		$decoderawtransaction = $bitcoin->decoderawtransaction($getrawtransaction);
+//		print_r($decoderawtransaction);
+		
+		$getblock['txid'][$txid] = $decoderawtransaction;
+		$txid ++;
+	}
+	
+	Blocks::create()->save($getblock);
+//	print_r($getblock);
+	}
+}
+?>
---
+++ app/models/Blocks.php
@@ -0,0 +1,7 @@
+<?php
+namespace app\models;
+
+class Blocks extends \lithium\data\Model {
+
+}
+?>
