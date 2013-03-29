e00fa7420db9b42ddf0ab81ccc5723f4d9a57ddc
Nilam Doctor <nilamdoc@gmail.com>
2013-03-29 15:31:05 +0530

    check

--- app/extensions/command/Tran.php
+++ app/extensions/command/Tran.php
@@ -10,7 +10,7 @@ class Tran extends \lithium\console\Command {
     public function run() {
 		$bitcoin = new Bitcoin('http://'.BITCOIN_WALLET_SERVER.':'.BITCOIN_WALLET_PORT,BITCOIN_WALLET_USERNAME,BITCOIN_WALLET_PASSWORD);
 		Transactions::remove();
-		$transactions = $bitcoin->listsinceblock(0);
+		$transactions = $bitcoin->listsinceblock();
 
 			foreach($transactions['transactions'] as $t){
 				$data = array(
