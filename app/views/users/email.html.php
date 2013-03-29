b0a2c0cde38be19e0cc1451f7af9ce88b506b78c
Nilam Doctor <nilamdoc@gmail.com>
2013-03-29 15:40:09 +0530

    spam

--- app/views/users/email.html.php
+++ app/views/users/email.html.php
@@ -5,3 +5,4 @@
 <?=$this->form->field('verified', array('type' => 'text', 'label'=>'Verification code','placeholder'=>'50d54d309d5d0c3423000000' )); ?>
 <?=$this->form->submit('Verify',array('class'=>'btn btn-primary')); ?>
 <?=$this->form->end(); ?>
+<p>Please check your spam folder too while checking your inbox!</p>
