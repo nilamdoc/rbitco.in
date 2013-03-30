f2c954d516394e7ae1e7927327228439c245a0bc
Nilam Doctor <nilamdoc@gmail.com>
2013-03-30 16:07:59 +0530

    comm

--- app/webroot/js/rbitcoin.js
+++ app/webroot/js/rbitcoin.js
@@ -62,8 +62,8 @@ function CompareAmount(){
 		alert("Transfer amount should be less than "+maxAmount);
 		return false;
 	}
-	if(Amount<1){
-		alert("Transfer cannot be less than 1 BTC. It will attract transfer charges.");
+	if(Amount<0.5){
+		alert("Transfer cannot be less than 0.5 BTC. It will attract transfer charges.");
 		return false;
 	}
 
