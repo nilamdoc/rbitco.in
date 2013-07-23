ea10addb7f615ffde8988d2a3771278d0adb30d1
Nilam Doctor <nilamdoc@gmail.com>
2013-07-23 14:37:45 +0530

    div

--- app/extensions/action/Functions.php
+++ app/extensions/action/Functions.php
@@ -111,7 +111,7 @@ curl_close($ch);
 $jdec = json_decode($json);
 //print_r($jdec->final_balance);
 // exit;
-			return round($jdec->final_balance/100000000,-8);
+			return $jdec->final_balance/100000000;
 	}
 
 	public function addressTransactions($address=null){
